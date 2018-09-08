99140c8e996f17979650134f3f16633fd26bb4f9
Igor Metko <metko21neon@gmail.com>
2018-09-08 09:18:45 +0300

    app work!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StoreComponent } from './store/store/store.component';
+import { CheckoutComponent } from './store/checkout/checkout.component';
+import { DetailsComponent } from './store/details/details.component';
 
-const routes: Routes = [];
+import { StoreGuard } from './store/store.guard';
+
+const routes: Routes = [
+  { path: 'store', component: StoreComponent, canActivate: [StoreGuard] },
+  { path: 'cart', component: DetailsComponent, canActivate: [StoreGuard] },
+  { path: 'checkout', component: CheckoutComponent, canActivate: [StoreGuard] },
+  { path: 'admin', loadChildren: 'src/app/admin/admin.module#AdminModule', canActivate: [StoreGuard] },
+  { path: '**', redirectTo: '/store' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  providers: [ StoreGuard ],
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { StoreModule } from './store/store.module';
+import { MaterialModule } from './material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    StoreModule,
+    MaterialModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
